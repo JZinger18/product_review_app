63d5d697aa631c7de9822633f87a7201cc68ed2c
Andrew Williams <andrewwilliams1995@Andrews-MacBook-Pro.local>
2017-10-13 00:46:13 -0400

    added test cases for new schema

--- models/User.js
+++ models/User.js
@@ -6,42 +6,32 @@ module.exports = function(sequelize, DataTypes)
     username: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique:true,
       validate: {
       len:[1,50]
       }
     },
     email:{
-      type: DataTypes.STRING,
-      allowNull: true,
-      validate:{
-        is: /^[^@]+@[^@]+.[^@]{3,6}$/i,
-        len: [1,50]
-      },
-      defaultValue:null,
-      unique:true
-    },
-      status: 
-      {
+          type: DataTypes.STRING,
+          allowNull: true,
+          validate:{
+            is: /^[^@]+@[^@]+.[^@]{3,6}$/i,
+            len: [1,50]
+          },
+        },
+    status: 
+          {
         type: DataTypes.ENUM('active', 'inactive'),
         defaultValue: 'active'
-        },
-      fbId : 
+          },
+    fbId : 
       {
         type: DataTypes.STRING,
         allowNull: false,
-        unique:true,
         primaryKey: true
       }
   });
 
     User.associate = function(models) {
-
-    User.hasMany(models.Review, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
 
     User.hasMany(models.Channel, {
       foreignKey: {

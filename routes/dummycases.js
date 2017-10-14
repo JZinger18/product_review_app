bb112036e968beee95a66da9b5fc9f3747ee4ee2
Andrew Williams <andrewwilliams1995@Andrews-MBP.the-beach.net>
2017-10-14 08:21:54 -0400

    heroku

--- routes/dummycases.js
+++ routes/dummycases.js
@@ -4,7 +4,7 @@
      module.exports = function(app)
      {
 
-      app.get("/adduserValues",function(req,res){
+      app.get("/",function(req,res){
 
      db.User.create({
       username:"andrewwilliams",

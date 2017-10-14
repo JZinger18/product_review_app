26c558f5212f0c2bbc2d1978fd87e6a1c1594c4d
Jordan Zinger <jzinger18@gmail.com>
2017-10-14 13:30:20 -0400

    heroku config

--- models/index.js
+++ models/index.js
@@ -4,14 +4,14 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
+var env       = process.env[config.use_env_variable]|| 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize('mysql://gxtgs6gr2e30wx9t:d0x6kcyje76loc1u@enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/uac7mm4av9uubu43')
 }
 
 fs

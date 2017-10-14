3a551a5000f82904166f87839414308e8e0cc478
Andrew Williams <andrewwilliams1995@Andrews-MacBook-Pro.local>
2017-10-13 23:28:41 -0400

    last url visited

--- routes/testcases.js
+++ routes/testcases.js
@@ -11,10 +11,9 @@ app.get("/addtestValues",function(req,res){
 		      category:"comedy"+i,
 		      thumbnail:"https://yt3.ggpht.com/-QWMKBXNBE2E/AAAAAAAAAAI/AAAAAAAAAAA/rEARmBXfgHw/s88-c-k-no-mo-rj-c0xffffff/photo.jpg",
 		      userFbId: "test"+i,
-		      amountOfStars:`<i class="fa fa-star"></i>`.repeat(Math.floor(Math.random()*6))
+		      amountOfStars:`<i class="fa fa-star"></i>`.repeat(1+Math.floor(Math.random()*5))
 		    }).then(function(hello){console.log(hello)})
 		}
 		res.json({done:"done"})
-
 	});
 }
--- server2.js
+++ server2.js
@@ -15,6 +15,26 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 
 var PORT = process.env.PORT || 8080;
 
+var cookieSession = require('cookie-session')
+var express = require('express')
+
+var app = express()
+
+app.set('trust proxy', 1) // trust first proxy
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
+
+app.get('/', function (req, res, next) {
+  // Update views
+  req.session.lastVisit = (req.session.views || 0) + 1
+
+  // Write response
+  res.end(req.session.views + ' views')
+})
+
 // Requiring our models for syncing
 // Sets up the Express app to handle data parsing
 
